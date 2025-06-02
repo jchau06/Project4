5f9a11979d967e75cee24ffbd471d2699b71285d
Jonathan Chau <chaujt1@uci.edu>
2025-06-01 22:14:14 -0700

    Prepared an early skeleton of the classes needed to run the project.

---
+++ grammar_classes.py
@@ -0,0 +1,36 @@
+
+class Symbol:
+    def generate(self, grammar):
+        pass
+
+class TerminalSymbol(Symbol):
+    def __init__(self):
+        pass
+
+    def generate(self, grammar):
+        pass
+
+class VariableSymbol(Symbol):
+    def __init__(self):
+        pass
+
+    def generate(self, grammar):
+        pass
+
+class Option:
+    def __init__(self):
+        pass
+
+    def generate(self, grammar):
+        pass
+
+class Rule:
+    def __init__(self):
+       pass
+
+    def generate(self, grammar):
+        pass
+
+class Grammar:
+    def __init__(self):
+        pass
